de7e173b690764dce01ccc4de8c62fc8c99ba6b7
CYLOH <malle.alstan@gmail.com>
2019-04-24 15:31:45 +0800

    Implement basic HomeViewModel with data binding

--- app/src/main/java/com/john/stylish/ui/home/FragHome.kt
+++ app/src/main/java/com/john/stylish/ui/home/FragHome.kt
@@ -1,16 +1,31 @@
 package com.john.stylish.ui.home
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.john.stylish.R
+import com.john.stylish.databinding.FragHomeBinding
 
 class FragHome: Fragment(){
 
+    lateinit var mHomeViewModel: HomeViewModel
+    lateinit var mFragHomeBinding: FragHomeBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var root = inflater.inflate(R.layout.frag_home, container, false);
+        return setDataBinding(inflater, container)
+    }
+
+    private fun setDataBinding(inflater: LayoutInflater, container: ViewGroup?): View {
+        mHomeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        mFragHomeBinding = DataBindingUtil.inflate(inflater, R.layout.frag_home, container , false)
+        mFragHomeBinding.homeViewModel = mHomeViewModel
+        mFragHomeBinding.setLifecycleOwner(this)
+
+        var root : View  = mFragHomeBinding.root
 
         return root
     }
--- app/src/main/java/com/john/stylish/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/john/stylish/ui/home/HomeViewModel.kt
@@ -1,5 +1,9 @@
 package com.john.stylish.ui.home
 
 import android.arch.lifecycle.ViewModel
+import com.john.stylish.utils.Constants.Companion.APP_NAME
 
-class HomeViewModel : ViewModel()
+class HomeViewModel : ViewModel() {
+
+    var mTitle = APP_NAME
+}
